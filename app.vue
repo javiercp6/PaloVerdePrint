a2a4166e2520d12a0694b3a6d0834a97b652078b
javiercp6 <javiercp@estudiantes.uci.cu>
2023-08-28 22:54:16 -0400

    update

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <NuxtLayout>
-    <NuxtPage class="dark:bg-gray-900" style="min-height: calc(100vh - 66px)" />
+    <NuxtPage keepalive class="dark:bg-gray-900" style="min-height: calc(100vh - 66px)" />
   </NuxtLayout>
 </template>
