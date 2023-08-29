d635fa392b2a89e385ca8abc1371d8ea2b2d2e54
javiercp6 <javiercp@estudiantes.uci.cu>
2023-08-28 22:28:22 -0400

    remove keepalive

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <NuxtLayout>
-    <NuxtPage keepalive class="dark:bg-gray-900" style="min-height: calc(100vh - 66px)" />
+    <NuxtPage class="dark:bg-gray-900" style="min-height: calc(100vh - 66px)" />
   </NuxtLayout>
 </template>
