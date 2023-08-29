e8a3377cc5f0318ce6c4000fbee5c963b2a9f75e
javiercp6 <javiercp@estudiantes.uci.cu>
2023-08-28 23:01:43 -0400

    update

--- pages/offer/index.vue
+++ pages/offer/index.vue
@@ -20,7 +20,6 @@
 //import { useMyFetch } from "~/composables/useMyFetch";
 const { pending, data: offers, error } = await useMyFetch("/offers", {
   lazy: true,
-  server: false,
 });
 
 /* const { data: offers, pending, error, refresh } = await useAsyncData("mountains", () =>
