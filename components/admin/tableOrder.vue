1b976a3ec3eb94dc0e43c56987df79b5defa2f6a
javiercp6 <javiercp@estudiantes.uci.cu>
2023-10-08 00:54:28 -0400

    Orders Admin

---
+++ components/admin/tableOrder.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="overflow-x-auto w-full">
+    <table class="table w-full">
+      <thead>
+        <tr>
+          <th>Order id</th>
+          <th>Order date</th>
+          <th>Total price</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="order in orders">
+          <td>
+            {{ order._id }}
+          </td>
+          <td>{{ order.createdAt }}</td>
+          <td>$ {{ order.paid / 100 }}</td>
+          <td>
+            <div class="badge badge-outline" :class="getBadge(order.status)">
+              {{ order.status }}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Order } from "@/types/interface";
+const props = defineProps<{ orders: Order[] }>();
+const { orders } = toRefs(props);
+
+const getBadge = (status: string) => {
+  switch (status) {
+    case "Pending":
+      return "";
+    case "processing":
+      return "badge-secondary";
+    case "Paid":
+      return "badge-accent";
+    case "cancelled":
+      return "badge-error";
+    default:
+      return "";
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ pages/admin/order.vue
@@ -0,0 +1,28 @@
+import { AdminTableUser } from '../../.nuxt/components';
+<template>
+  <main class="flex-1 overflow-y-auto pt-8 px-6">
+    <AdminTableLayout title-btn="Add Order" title="Orders">
+      <template v-slot:tableBody>
+        <AdminTableOrder v-if="data?.data && !pending && !error" :orders="data.data" />
+        <span v-if="pending" class="loading loading-ring loading-lg"></span>
+        <WidgetsError v-if="error && !pending" :error="error" @refresh="refresh()" />
+      </template>
+      <template v-slot:pagination>
+        <WidgetsPagination :page="page" :totalPage="data?.totalPages!" />
+      </template>
+    </AdminTableLayout>
+  </main>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "admin",
+  middleware: ["admin"],
+});
+const route = useRoute();
+const { page } = useUtils();
+const { getOrders } = useOrder();
+const { data, pending, error, refresh } = await getOrders();
+</script>
+
+<style scoped></style>
