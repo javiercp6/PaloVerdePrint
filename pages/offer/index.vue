41c1893cdc181ac34707d0bbac42a5863021c155
javiercp6 <javiercp@estudiantes.uci.cu>
2023-10-29 16:43:59 -0400

    Cache de offer

--- pages/offer/index.vue
+++ pages/offer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="mx-auto px-2 2xl:px-10 container flex flex-col justify-center w-full">
-    <div v-if="!pending && data" class="w-full">
+    <div v-if="data" class="w-full">
       <div
         class="grid gap-4 gap-x-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-content-center py-10"
       >
@@ -9,9 +9,9 @@
         <WidgetsOfferCard v-for="offer in data?.data" :offer="offer" />
       </div>
     </div>
-    <WidgetsLoading v-if="pending" />
-    <WidgetsError v-if="error && !pending" :error="error" @refresh="refresh()" />
-    <div v-if="!pending && data" class="flex justify-center">
+    <WidgetsLoading v-else-if="pending" />
+    <WidgetsError v-else-if="error" :error="error" @refresh="refresh()" />
+    <div v-if="data" class="flex justify-center">
       <WidgetsPagination :page="page" :totalPage="data?.totalPages!" />
     </div>
     <!--     <button class="btn btn-primary btn-sm text-xs" @click="wish" type="submit">
